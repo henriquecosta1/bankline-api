4207fd2e475f0f35537857c6b6ca5b41a18d1377
unknown <fabio.costa@trt8.jus.br>
2022-05-04 23:56:37 -0300

    Comitando classes modelo

---
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -0,0 +1,23 @@
+package com.dio.santander.bankline.api.model;
+
+public class Conta {
+	
+	private Long numero;
+	private Double saldo;
+	
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+
+}
